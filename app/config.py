5baff2b529e433c7d95e5084a1c33a6540566214
djoni@DESKTOP-2NGN06F <djoni84@inbox.ru>
2024-12-29 13:28:05 +0100

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config(object):
+    USER = os.environ.get('USER','djoni')
+    PASSWORD = os.environ.get('PASSWORD', 'Nehby1984!')
+    HOST = os.environ.get('HOST', '127.0.0.1')
+    PORT = os.environ.get('PORT','5532')
+    DB = os.environ.get('DB','mydb')
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
---
+++ app/routes/post.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+from ..extensions import db
+from ..models.post import Post
+
+post = Blueprint("post", __name__)
+
+@post.route("/post/<subject>")
+def create_post(subject):
+    post = Post(subject=subject)
+    db.session.add(post)
+    db.session.commit()
+    return f"Post created {post.subject}"
